240b016c201f4b20290fb166b1f530df085ee278
Dpbm <dpbm136@gmail.com>
2024-09-28 14:54:52 -0300

    feat: add tests for CX gate

--- versions/Lua/tests.lua
+++ versions/Lua/tests.lua
@@ -1,6 +1,78 @@
 
 dofile("MicroMoth.lua")
 
+function test_cx00 ()
+	local qc = QuantumCircuit()
+	local statevec, is_desired_state, has_no_phase
+
+	qc.set_registers(2)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 1 and statevec[2][1] == 0 and statevec[3][1] == 0 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Initial State for CX 00")
+	
+	qc.cx(0,1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 1 and statevec[2][1] == 0 and statevec[3][1] == 0 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Final State for CX 00")
+end
+
+function test_cx01 ()
+	local qc = QuantumCircuit()
+	local statevec, is_desired_state, has_no_phase
+
+	qc.set_registers(2)
+	qc.x(0)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 1 and statevec[3][1] == 0 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Initial State for CX 01")
+	
+	qc.cx(0,1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 0 and statevec[3][1] == 0 and statevec[4][1] == 1
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Final State for CX 01")
+end
+
+function test_cx10 ()
+	local qc = QuantumCircuit()
+	local statevec, is_desired_state, has_no_phase
+
+	qc.set_registers(2)
+	qc.x(1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 0 and statevec[3][1] == 1 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Initial State for CX 10")
+	
+	qc.cx(0,1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 0 and statevec[3][1] == 1 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Final State for CX 10")
+end
+
+function test_cx11 ()
+	local qc = QuantumCircuit()
+	local statevec, is_desired_state, has_no_phase
+
+	qc.set_registers(2)
+	qc.x(0)
+	qc.x(1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 0 and statevec[3][1] == 0 and statevec[4][1] == 1
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Initial State for CX 11")
+	
+	qc.cx(0,1)
+	statevec = simulate(qc, "statevector")
+	is_desired_state = statevec[1][1] == 0 and statevec[2][1] == 1 and statevec[3][1] == 0 and statevec[4][1] == 0
+	has_no_phase = statevec[1][2] == 0 and statevec[2][2] == 0 and statevec[3][2] == 0 and statevec[4][2] == 0
+	assert(is_desired_state and has_no_phase , "Invalid Final State for CX 11")
+end
+
 function test_swap00 ()
 	local qc = QuantumCircuit()
 	local statevec, is_desired_state, has_no_phase
@@ -75,6 +147,12 @@ function test_swap11 ()
 end
 
 print("Doing Tests...")
+
+
+test_cx00()
+test_cx01()
+test_cx10()
+test_cx11()
 
 test_swap00()
 test_swap01()
