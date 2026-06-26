3063067b04e6e5d37aa07d47e39d61e178857ecd
Astryd Park <artreadcode@gmail.com>
2026-06-26 22:26:45 +0100

    Fixed the bug related to Assembly builds
    Assembly now builds as `Moth.MicroMoth.dll` (was `MicroMoth.dll`) — distinct from any test project named exactly the same as the main library, `micromoth`, thus avoiding the assembly collision from the same `micromoth.dll`.

--- versions/Csharp/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ versions/Csharp/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d22729365d632845fd39da3c5dddcabbeb62e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18a0ab90e5563d1d4ea786f9662caa32e8330cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
