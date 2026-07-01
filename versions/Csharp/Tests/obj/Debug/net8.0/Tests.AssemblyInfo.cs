87c2ea9c977ee1c731e3746510419a2b1d819f1e
Astryd Park <astryd@mothquantum.com>
2026-07-01 11:20:54 +0100

    brought updated versions

--- versions/Csharp/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ versions/Csharp/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18a0ab90e5563d1d4ea786f9662caa32e8330cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3063067b04e6e5d37aa07d47e39d61e178857ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
